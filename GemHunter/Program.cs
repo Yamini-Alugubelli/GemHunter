0236d1cc0b0b2dd96852b5212c0d38a1790e4909
Yamini Alugubelli <yaminireddyallugubeli.70@gmail.com>
2024-02-15 23:46:22 -0500

    Implemented Move() method

--- GemHunter/Program.cs
+++ GemHunter/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Position
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Position(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+public class Player
+{
+    public string Name { get; }
+    public Position Position { get; set; }
+    public int GemCount { get; set; }
+
+    public Player(string name, Position position)
+    {
+        Name = name;
+        Position = position;
+        GemCount = 0;
+    }
+
+    public void Move(char direction)
+    {
+        switch (direction)
+        {
+            case 'U':
+                Position.Y--;
+                break;
+            case 'D':
+                Position.Y++;
+                break;
+            case 'L':
+                Position.X--;
+                break;
+            case 'R':
+                Position.X++;
+                break;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+      
+    }
+}
+
