447045eb225309d7d3adf884be181c8063d545cb
Yamini Alugubelli <yaminireddyallugubeli.70@gmail.com>
2024-02-18 21:08:47 -0500

    Implemented Braod() method

--- GemHunter/Program.cs
+++ GemHunter/Program.cs
@@ -43,8 +43,43 @@ public class Player
         }
     }
 }
+public class Cell
+{
+    public string Occupant { get; set; }
 
-class Program
+    public Cell(string occupant)
+    {
+        Occupant = occupant;
+    }
+}
+
+public class Board
+{
+    public Cell[,] Grid { get; }
+
+    public Board()
+    {
+        Grid = new Cell[6, 6];
+        // Initialize the board with empty spaces
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                Grid[i, j] = new Cell("-");
+            }
+        }
+        // Initialize players and obstacles
+        Grid[0, 0].Occupant = "P1";
+        Grid[5, 5].Occupant = "P2";
+        // Initialize gems (you can randomize the positions)
+        Grid[2, 2].Occupant = "G";
+        Grid[4, 4].Occupant = "G";
+        // Initialize obstacles (you can randomize the positions)
+        Grid[1, 1].Occupant = "O";
+        Grid[3, 3].Occupant = "O";
+    }
+}
+    class Program
 {
     static void Main()
     {
