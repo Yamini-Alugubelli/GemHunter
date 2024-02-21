f5819603c0ffb403028b27604d9fb81274f09e82
Yamini Alugubelli <yaminireddyallugubeli.70@gmail.com>
2024-02-20 21:56:34 -0500

    Implemented CollectGem() ,display()

--- GemHunter/Program.cs
+++ GemHunter/Program.cs
@@ -78,6 +78,51 @@ public class Board
         Grid[1, 1].Occupant = "O";
         Grid[3, 3].Occupant = "O";
     }
+    public void Display()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                Console.Write(Grid[i, j].Occupant + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+    public bool IsValidMove(Player player, char direction)
+    {
+        int newX = player.Position.X;
+        int newY = player.Position.Y;
+
+        switch (direction)
+        {
+            case 'U':
+                newY--;
+                break;
+            case 'D':
+                newY++;
+                break;
+            case 'L':
+                newX--;
+                break;
+            case 'R':
+                newX++;
+                break;
+        }
+
+        // Check if the new position is within the bounds and not an obstacle
+        return newX >= 0 && newX < 6 && newY >= 0 && newY < 6 && Grid[newY, newX].Occupant != "O";
+    }
+
+    public void CollectGem(Player player)
+    {
+        if (Grid[player.Position.Y, player.Position.X].Occupant == "G")
+        {
+            player.GemCount++;
+            Grid[player.Position.Y, player.Position.X].Occupant = "-";
+        }
+    }
+
 }
     class Program
 {
